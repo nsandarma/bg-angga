8acdc34cd7836151d96015a421dd00ad6cc2e48f
nsandarma <nsandarma@gmail.com>
2023-01-22 16:01:53 +0700

    add resource

---
+++ web-flask/req.py
@@ -0,0 +1,10 @@
+import pandas 
+import sklearn
+import joblib
+import flask
+
+print(f"{pandas.__name__}=={pandas.__version__}")
+print(f"{sklearn.__name__}=={sklearn.__version__}")
+print(f"{joblib.__name__}=={joblib.__version__}")
+print(f"{flask.__name__}=={flask.__version__}")
+
